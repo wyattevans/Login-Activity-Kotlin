49933f16cc9b889df9f1861da19e91ffbe69f187
Wyatt Evans <syattevans@gmail.com>
2020-07-15 08:37:48 -0600

    Removed comments from LoginRepository class.

--- app/src/main/java/com/app/loginactivitykotlin/data/LoginRepository.kt
+++ app/src/main/java/com/app/loginactivitykotlin/data/LoginRepository.kt
@@ -2,14 +2,8 @@ package com.app.loginactivitykotlin.data
 
 import com.app.loginactivitykotlin.data.model.LoggedInUser
 
-/**
- * Class that requests authentication and user information from the remote data source and
- * maintains an in-memory cache of login status and user credentials information.
- */
-
 class LoginRepository(val dataSource: LoginDataSource) {
 
-    // in-memory cache of the loggedInUser object
     var user: LoggedInUser? = null
         private set
 
@@ -17,8 +11,6 @@ class LoginRepository(val dataSource: LoginDataSource) {
         get() = user != null
 
     init {
-        // If user credentials will be cached in local storage, it is recommended it be encrypted
-        // @see https://developer.android.com/training/articles/keystore
         user = null
     }
 
@@ -28,7 +20,6 @@ class LoginRepository(val dataSource: LoginDataSource) {
     }
 
     fun login(username: String, password: String): Result<LoggedInUser> {
-        // handle login
         val result = dataSource.login(username, password)
 
         if (result is Result.Success) {
@@ -40,7 +31,5 @@ class LoginRepository(val dataSource: LoginDataSource) {
 
     private fun setLoggedInUser(loggedInUser: LoggedInUser) {
         this.user = loggedInUser
-        // If user credentials will be cached in local storage, it is recommended it be encrypted
-        // @see https://developer.android.com/training/articles/keystore
     }
 }
