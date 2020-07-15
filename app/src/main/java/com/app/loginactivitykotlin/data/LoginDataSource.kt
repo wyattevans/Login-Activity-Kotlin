f620f22b593d992d648fe2c3936ea8d678198bb6
Wyatt Evans <syattevans@gmail.com>
2020-07-15 08:36:53 -0600

    Optimized try-catch inside login function.

--- app/src/main/java/com/app/loginactivitykotlin/data/LoginDataSource.kt
+++ app/src/main/java/com/app/loginactivitykotlin/data/LoginDataSource.kt
@@ -6,11 +6,11 @@ import java.io.IOException
 class LoginDataSource {
 
     fun login(username: String, password: String): Result<LoggedInUser> {
-        try {
+        return try {
             val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "Jane Doe")
-            return Result.Success(fakeUser)
+            Result.Success(fakeUser)
         } catch (e: Throwable) {
-            return Result.Error(IOException("Error logging in", e))
+            Result.Error(IOException("Error logging in", e))
         }
     }
 
