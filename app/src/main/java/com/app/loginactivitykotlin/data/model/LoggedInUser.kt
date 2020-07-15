76a01ef33139f0910b7ef869e661b07e502ef08e
Wyatt Evans <syattevans@gmail.com>
2020-07-15 08:34:51 -0600

    Removed comments from LoggedInUser data class.

--- app/src/main/java/com/app/loginactivitykotlin/data/model/LoggedInUser.kt
+++ app/src/main/java/com/app/loginactivitykotlin/data/model/LoggedInUser.kt
@@ -1,8 +1,5 @@
 package com.app.loginactivitykotlin.data.model
 
-/**
- * Data class that captures user information for logged in users retrieved from LoginRepository
- */
 data class LoggedInUser(
         val userId: String,
         val displayName: String
