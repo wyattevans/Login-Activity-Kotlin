9a5da44c9698a3d52395de812d3692ec092fadcb
Wyatt Evans <syattevans@gmail.com>
2020-07-17 00:04:10 -0600

    Removed comments from result class.

--- app/src/main/java/com/app/loginactivitykotlin/data/Result.kt
+++ app/src/main/java/com/app/loginactivitykotlin/data/Result.kt
@@ -1,9 +1,5 @@
 package com.app.loginactivitykotlin.data
 
-/**
- * A generic class that holds a value with its loading status.
- * @param <T>
- */
 sealed class Result<out T : Any> {
 
     data class Success<out T : Any>(val data: T) : Result<T>()
