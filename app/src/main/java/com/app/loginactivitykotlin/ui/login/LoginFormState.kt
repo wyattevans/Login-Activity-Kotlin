fa786798a3d2efd9ed304ad3f7f89e3773758ed3
Wyatt Evans <syattevans@gmail.com>
2020-07-18 11:12:24 -0600

    Removed comments from login form state.

--- app/src/main/java/com/app/loginactivitykotlin/ui/login/LoginFormState.kt
+++ app/src/main/java/com/app/loginactivitykotlin/ui/login/LoginFormState.kt
@@ -1,8 +1,5 @@
 package com.app.loginactivitykotlin.ui.login
 
-/**
- * Data validation state of the login form.
- */
 data class LoginFormState(val usernameError: Int? = null,
                           val passwordError: Int? = null,
                           val isDataValid: Boolean = false)
