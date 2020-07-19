13f297c1819799105f8f79965e821b0ca2cb5486
Wyatt Evans <syattevans@gmail.com>
2020-07-19 10:41:43 -0600

    Removed comments from Login View Model Factory.

--- app/src/main/java/com/app/loginactivitykotlin/ui/login/LoginViewModelFactory.kt
+++ app/src/main/java/com/app/loginactivitykotlin/ui/login/LoginViewModelFactory.kt
@@ -5,10 +5,6 @@ import androidx.lifecycle.ViewModelProvider
 import com.app.loginactivitykotlin.data.LoginDataSource
 import com.app.loginactivitykotlin.data.LoginRepository
 
-/**
- * ViewModel provider factory to instantiate LoginViewModel.
- * Required given LoginViewModel has a non-empty constructor
- */
 class LoginViewModelFactory : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
